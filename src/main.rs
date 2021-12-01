2f3ab9c7a8c4799006a36902fd4e5cda54d37ed5
TheBlackfurGuy <64478051+TheBlackfurGuy@users.noreply.github.com>
2021-12-01 18:41:36 +0100

    Day 1 Part 2 also done :D

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ fn main() {
             {
                 "2021-1-1": day_one_part_one
             }
+            {
+                "2021-1-2": day_one_part_two
+            }
         ]
     }
 }
@@ -21,5 +24,25 @@ fn day_one_part_one(input: &str) -> String {
         }
         previous = number;
     }
+    count.to_string()
+}
+
+fn day_one_part_two(input: &str) -> String {
+    let mut sums: Vec<u16> = vec![];
+    let numbers: Vec<u16> = input.split_terminator('\n').map(|x| x.parse::<u16>().unwrap()).collect();
+    let mut previous = u16::MAX;
+    let mut count = 0;
+
+    for i in 0..=numbers.len()-3 {
+        sums.push(numbers[i..i+3].iter().sum());
+    }
+
+    for number in sums {
+        if number > previous {
+            count += 1;
+        }
+        previous = number;
+    }
+
     count.to_string()
 }
