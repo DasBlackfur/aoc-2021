78e45257100bc6433c85f23287f506f120eb57c8
TheBlackfurGuy <64478051+TheBlackfurGuy@users.noreply.github.com>
2021-12-02 16:35:37 +0100

    day 2 part 1 done

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,15 @@ fn main() {
             {
                 "2021-1-2": day_one_part_two
             }
+            {
+                "2021-2-1": day_two_part_one
+            }
         ]
     }
 }
 
 fn day_one_part_one(input: &str) -> String {
-    let numbers: Vec<u16> = input.split_terminator('\n').map(|x| x.parse::<u16>().unwrap()).collect();
+    let numbers: Vec<u16> = input.lines().map(|x| x.parse::<u16>().unwrap()).collect();
     let mut previous = u16::MAX;
     let mut count = 0;
 
@@ -29,12 +32,12 @@ fn day_one_part_one(input: &str) -> String {
 
 fn day_one_part_two(input: &str) -> String {
     let mut sums: Vec<u16> = vec![];
-    let numbers: Vec<u16> = input.split_terminator('\n').map(|x| x.parse::<u16>().unwrap()).collect();
+    let numbers: Vec<u16> = input.lines().map(|x| x.parse::<u16>().unwrap()).collect();
     let mut previous = u16::MAX;
     let mut count = 0;
 
-    for i in 0..=numbers.len()-3 {
-        sums.push(numbers[i..i+3].iter().sum());
+    for i in 0..=numbers.len() - 3 {
+        sums.push(numbers[i..i + 3].iter().sum());
     }
 
     for number in sums {
@@ -45,4 +48,24 @@ fn day_one_part_two(input: &str) -> String {
     }
 
     count.to_string()
+}
+
+fn day_two_part_one(input: &str) -> String {
+    let mut depth: u16 = 0;
+    let mut pos: u16 = 0;
+
+    for line in input.lines() {
+        let arr = line.split(' ').collect::<Vec<&str>>();
+        match (
+            arr.get(0).unwrap(),
+            arr.get(1).unwrap().parse::<u16>().unwrap(),
+        ) {
+            (&"forward", i) => pos += i,
+
+            (&"down", i) => depth += i,
+            (&"up", i) => depth -= i,
+            (_, _) => panic!("WHAAAAT?"),
+        }
+    }
+    (depth as u64 * pos as u64).to_string()
 }
